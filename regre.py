0a6c82a086a6850469befc08e71ce1d4faf735e4
Diego Tumba <a20203402@pucp.edu.pe>
2026-02-21 09:40:13 -0500

    Add files via upload

---
+++ regre.py
@@ -0,0 +1,25 @@
+# podriamos deducir el significado de cada variable 
+# mirando el dataset en csv file
+
+# Variables numéricas continuas
+numericas = {
+    'A2': 'Edad',
+    'A3': 'Deuda_Actual',
+    'A5': 'Años_Empleo',
+    'A6': 'Score_Crédito',
+    'A7': 'Monto_Solicitado',
+    'A14': 'Créditos_Existentes'
+}
+
+# Variables categóricas (para gráficos de barras)
+categoricas = {
+    'A1': 'Género',
+    'A4': 'Estado_Civil',
+    'A8': 'Tiene_Ahorros',
+    'A9': 'Incumplimiento_Previo',
+    'A10': 'Estabilidad_Laboral',
+    'A11': 'Propietario',
+    'A12': 'Tipo_Vivienda',
+    'A13': 'Propósito_Préstamo'
+}
+
